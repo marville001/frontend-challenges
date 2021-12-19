6a33a7b598841678df8518056d2613a23d091608
Martin Mwangi <mwangimartin1904@gmail.com>
2021-12-19 21:15:51 +0300

    Load challenges

--- src/pages/Challenges.jsx
+++ src/pages/Challenges.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ChallengeCard from "../components/ChallengeCard";
 
+import data from "../data.json"
+
 const Challenges = () => {
   return (
     <div className="container py-12">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {[1, 2, 3, 4, 5, 6, 7].map((_, i) => (
+        {data.challenges.map((_, i) => (
           <ChallengeCard key={i} />
         ))}
       </div>
