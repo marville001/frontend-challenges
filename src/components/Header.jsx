9c978a7aefffdfd5f54dc2f820ab37ec4593658d
Martin Mwangi <mwangimartin1904@gmail.com>
2021-12-19 20:31:53 +0300

    header design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>
-            <h5>Header</h5>
+        <div className="bg-indigo-500">
+
+        <div className='flex items-center h-16 px-4 lg:px-0  container justify-between'>
+            <h1 className='font-bold text-slate-100 font-sans'>By @Martin</h1>
+            <div>
+                <button className='bg-slate-100 rounded-full px-4 py-1 text-indigo-500 font-bold shadow-lg hover:bg-slate-200 hover:text-indigo-600'>Hire Me</button>
+            </div>
+        </div>
         </div>
     )
 }
