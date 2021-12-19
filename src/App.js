9a451ee6c5e1fb328622fcdaa0e101e9443cb4eb
Martin Mwangi <mwangimartin1904@gmail.com>
2021-12-19 20:37:44 +0300

    Header and footer design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Footer from './components/Footer'
 import Header from './components/Header'
 
 const App = () => {
   return (
     <div className='bg-gray-100 h-screen'>
       <Header />
+      <div className='min-h-full'>
+
+      </div>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="bg-indigo-500">
+
+        <div className='h-12 px-4 container flex justify-center flex-col'>
+            <p className='text-center text-slate-100'>Copyright @ Martin - mwangimartin1904@gmail.com</p>
+        </div>
+        </div>
+    )
+}
+
+export default Footer
