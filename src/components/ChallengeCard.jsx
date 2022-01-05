8fad3e3f52280a429b3d275c8f51ac55dd063b91
Martin Mwangi <mwangimartin1904@gmail.com>
2022-01-05 12:03:54 +0300

    Updates

--- src/components/ChallengeCard.jsx
+++ src/components/ChallengeCard.jsx
@@ -4,27 +4,41 @@ import { FaGithub } from "react-icons/fa";
 
 const ChallengeCard = ({ challenge, index }) => {
   return (
-    <Link to={`/challenge/${challenge.id}`} className="bg-slate-100 shadow-lg p-5 rounded-md">
-      <h2 className="text-indigo-500"> <span className="text-indigo-300">#{index}</span> {challenge.name}</h2>
-      <img
-        src={challenge.image}
-        alt="Project XYZ"
-        className="rounded-md my-4 md:h-52 w-full"
-      />
+    <div
+      className="bg-slate-100 shadow-lg p-5 rounded-md"
+    >
+      <h2 className="text-indigo-500">
+        {" "}
+        <span className="text-indigo-300">#{index}</span> {challenge.name}
+      </h2>
+      <Link
+        to={`/challenge/${challenge.id}`}
+      >
+        <img
+          src={challenge.image}
+          alt="Project XYZ"
+          className="rounded-md my-4 md:h-52 w-full"
+        />
+      </Link>
       <div className="flex justify-between text-indigo-400">
-      <Link className="text-sm flex items-center" to={`/challenge/${challenge.id}`}>
+        <Link
+          className="text-sm flex items-center"
+          to={`/challenge/${challenge.id}`}
+        >
           <FaGithub className="mr-1" />
           View Challenge
         </Link>
-        <a 
-        target="_blank"
-        className="text-sm flex items-center" 
-        href="https://github.com/marville001" rel="noreferrer">
+        <a
+          target="_blank"
+          className="text-sm flex items-center"
+          href="https://github.com/marville001"
+          rel="noopener noreferrer"
+        >
           <FaGithub className="mr-1" />
           Source Code
         </a>
       </div>
-    </Link>
+    </div>
   );
 };
 
