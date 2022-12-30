af4b131413708d6c6a2002cd65fe3eee4e76ac34
Martin Mwangi <mwangimartin1904@gmail.com>
2022-12-30 18:47:05 +0300

    Update footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = () => {
   return (
     <div className="bg-indigo-500 mb-0">
       <div className="h-12 px-4 container flex justify-center flex-col">
-        <p className="text-center text-sm text-slate-100">
+        <a href="mailto:mwangimartin1904@gmail.com" className="text-center text-sm text-slate-100">
           Copyright @ Martin - mwangimartin1904@gmail.com
-        </p>
+        </a>
       </div>
     </div>
   );
